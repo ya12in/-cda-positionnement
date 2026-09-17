f7ec4aed7072f237a1f363b425ac22d554b1ca1f
yassin <sanji.vinsk@gmail.com>
2026-09-17 16:55:51 +0200

    feat: fin exo js

--- exercices/js-dom/todo.js
+++ exercices/js-dom/todo.js
@@ -1,32 +1,32 @@
-const btnclick = document.querySelector(".btn-ajout")
-const input =  document.querySelector(".input-list")
-const ul = document.querySelector('.ul-list')
-const footer =  document.querySelector("footer")
+const btnclick = document.querySelector(".btn-ajout");
+const input = document.querySelector(".input-list");
+const ul = document.querySelector(".ul-list");
+const footer = document.querySelector("footer");
 
-btnclick.addEventListener('click', () => {
+btnclick.addEventListener("click", () => {
+  if (input.value === "") {
+    return;
+  }
 
-    if (input.value === "") {
-        return
-    }
+  const li = document.createElement("li");
+  li.textContent = input.value;
 
-    const li = document.createElement('li')
-    li.textContent = input.value
-    
-    const btnDelete = document.createElement('button')
-    btnDelete.textContent = "Supprimer"
-    
-    li.appendChild(btnDelete)
-    ul.appendChild(li)
-    
-    input.value = ""
-    
-    btnDelete.addEventListener('click', () => {
-        li.remove()
-    })
+  const btnDelete = document.createElement("button");
+  btnDelete.textContent = "Supprimer";
 
-    li.addEventListener('click', () => {
-        li.style.textDecoration = "line-through"
-    })
+  li.appendChild(btnDelete);
+  ul.appendChild(li);
 
-    footer.textContent = "compteur de taches : " + ul.querySelectorAll('li').length
-}) 
+  input.value = "";
+
+  btnDelete.addEventListener("click", () => {
+    li.remove();
+  });
+
+  li.addEventListener("click", () => {
+    li.style.textDecoration = "line-through";
+  });
+
+  footer.textContent =
+    "compteur de taches : " + ul.querySelectorAll("li").length;
+});
